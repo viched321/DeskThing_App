b840f8fd566955c12b743628bf9ab2376924762b
victor <victor.s.hedlund@gmail.com>
2024-12-09 12:57:19 +0100

    Added files from old resp

---
+++ config.py
@@ -0,0 +1,4 @@
+CLIENT_ID = '1d2ba4e5cb9d4e8b8821544066449ce1'
+CLIENT_SECRET = '1dd12a8e100743f4a112c1c80eb62d4a'
+REDIRECT_URI = 'http://localhost:8080'
+SCOPE = 'user-read-currently-playing user-read-playback-state user-modify-playback-state playlist-read-private user-library-read user-top-read user-read-recently-played playlist-modify-private playlist-modify-public'
